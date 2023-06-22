89e6c7b1d1f6be789394120356000efe3d7892ce
tsviatkov <nick.tsviatkov@gmail.com>
2023-06-22 09:45:18 +0300

    Update version of openfort-node for ssv-social-nextjs sample; add prettier

--- ssv-social-nextjs/pages/api/examples/protected-register-session.ts
+++ ssv-social-nextjs/pages/api/examples/protected-register-session.ts
@@ -1,50 +1,43 @@
 // This is an example of to protect an API route
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "../auth/[...nextauth]";
+import {getServerSession} from "next-auth/next";
+import {authOptions} from "../auth/[...nextauth]";
 
-import type { NextApiRequest, NextApiResponse } from "next";
-import Openfort from "@openfort/openfort-node";
+import type {NextApiRequest, NextApiResponse} from "next";
+import Openfort, {CreatePlayerSessionRequest} from "@openfort/openfort-node";
 
 const openfort = new Openfort(process.env.NEXTAUTH_OPENFORT_SECRET_KEY!);
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const session = await getServerSession(req, res, authOptions);
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const session = await getServerSession(req, res, authOptions);
 
-  if (session) {
-    // Get the address of the session key.
-    const { address } = req.body;
+    if (session) {
+        // Get the address of the session key.
+        const {address} = req.body;
 
-    const policy_id = process.env.NEXTAUTH_OPENFORT_POLICY!;
-    const player_id = process.env.NEXTAUTH_OPENFORT_PLAYER!;
-    const valid_until = 281474976710655;
-    const valid_after = 0;
-    const chain_id = Number(process.env.NEXTAUTH_OPENFORT_CHAINID!);
+        const createSessionRequest: CreatePlayerSessionRequest = {
+            id: process.env.NEXTAUTH_OPENFORT_PLAYER!,
+            address: address!.toString(),
+            chain_id: Number(process.env.NEXTAUTH_OPENFORT_CHAINID!),
+            valid_until: 281474976710655,
+            valid_after: 0,
+            policy: process.env.NEXTAUTH_OPENFORT_POLICY!,
+        };
 
-    try {
-      const playerSession = await openfort.players.createPlayerSession(
-        player_id,
-        address!.toString(),
-        chain_id,
-        valid_until,
-        valid_after,
-        policy_id
-      );
+        try {
+            const playerSession = await openfort.players.createSession(createSessionRequest);
 
-      return res.send({
-        data: playerSession.body,
-      });
-    } catch (e: any) {
-      console.log(e.body);
-      return res.send({
-        data: null,
-      });
+            return res.send({
+                data: playerSession,
+            });
+        } catch (e: any) {
+            console.log(e);
+            return res.send({
+                data: null,
+            });
+        }
     }
-  }
 
-  res.send({
-    error: "You must be signed in to view the protected content on this page.",
-  });
+    res.send({
+        error: "You must be signed in to view the protected content on this page.",
+    });
 }
