fdaf797c8d3f5a069156666a2339712ec7b530c8
Eloi Manuel <eloi.manvie@gmail.com>
2023-08-08 16:10:23 +0200

    working on recoverable sample

---
+++ recoverable-accounts/pages/api/register-session.ts
@@ -0,0 +1,44 @@
+// This is an example of to protect an API route
+import {getServerSession} from "next-auth/next";
+import {getAuthOptions} from "./auth/[...nextauth]";
+
+import type {NextApiRequest, NextApiResponse} from "next";
+import Openfort, {CreatePlayerSessionRequest} from "@openfort/openfort-node";
+
+const openfort = new Openfort(process.env.NEXTAUTH_OPENFORT_SECRET_KEY!, "http://localhost:3000");
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const session = await getServerSession(req, res, getAuthOptions(req));
+
+    if (session) {
+        // Get the address of the session key.
+        const {address} = req.body;
+
+        const createSessionRequest: CreatePlayerSessionRequest = {
+            playerId: session.playerId,
+            address: address!.toString(),
+            chainId: Number(process.env.NEXTAUTH_OPENFORT_CHAINID!),
+            validUntil: 281474976710655,
+            validAfter: 0,
+            policy: process.env.NEXTAUTH_OPENFORT_POLICY!,
+            externalOwnerAddress: session.user?.name!,
+        };
+
+        try {
+            const playerSession = await openfort.players.createSession(createSessionRequest);
+
+            return res.send({
+                data: playerSession,
+            });
+        } catch (e: any) {
+            console.log(e.body);
+            return res.send({
+                data: null,
+            });
+        }
+    }
+
+    res.send({
+        error: "You must be signed in to view the protected content on this page.",
+    });
+}
