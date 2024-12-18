054a62397b99ae5f182fde0759fdf3fd01bf7cd1
joalavedra <62625514+joalavedra@users.noreply.github.com>
2024-12-18 17:22:22 +0100

    Update 2

--- oauth-game-ecosystem/ecosystem-backend/prisma/migrations/20230613093617_remove_description_game/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `description` on the `Game` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Game" DROP COLUMN "description";
