74ae8fac4f3a6bd85f4713aa9f326575bc7c705b
Huguet57 <andreuhuguet@gmail.com>
2025-09-17 10:15:39 +0200

    Policy Id is optional

--- aave/frontend/src/Providers.tsx
+++ aave/frontend/src/Providers.tsx
@@ -34,7 +34,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
               shieldPublishableKey: import.meta.env.VITE_OPENFORT_SHIELD_PUBLIC_KEY,
               createEncryptedSessionEndpoint: `${import.meta.env.VITE_BACKEND_URL}/api/create-encryption-session`,
               recoveryMethod: RecoveryMethod.PASSWORD,
-              ethereumProviderPolicyId: import.meta.env.VITE_OPENFORT_POLICY_ID,
+              ethereumProviderPolicyId: import.meta.env.VITE_OPENFORT_POLICY_ID || undefined,
             }}
           >
             {children}
