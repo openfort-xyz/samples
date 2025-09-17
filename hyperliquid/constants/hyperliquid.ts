e57937293317cb363acb516cc2367fc396a7a26e
Huguet57 <andreuhuguet@gmail.com>
2025-09-17 16:12:32 +0200

    Fix Hyperliquid USDC checksum address

--- hyperliquid/constants/hyperliquid.ts
+++ hyperliquid/constants/hyperliquid.ts
@@ -1,7 +1,7 @@
 export const HYPERLIQUID_TESTNET_HTTP_URL = "https://api.hyperliquid-testnet.xyz";
 export const HYPERLIQUID_TESTNET_WS_URL = "wss://api.hyperliquid-testnet.xyz/ws";
 
-export const HYPERLIQUID_USDC_TOKEN_ADDRESS = "0x75fAF114eafb1bDBE2F0316DF893fd58cE46AA4d" as const;
+export const HYPERLIQUID_USDC_TOKEN_ADDRESS = "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d" as const;
 export const HYPERLIQUID_USDC_DECIMALS = 6;
 
 export const HYPE_ASSET_ID = 107 as const;
